e7d25dad5cc86bd06a27b663351667c913cded42
seyeon102424 <149925644+seyeon102424@users.noreply.github.com>
2024-08-08 16:19:27 +0900

    [Bronze V] Title: ??!, Time: 168 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String name;
+
+        name = s.nextLine();
+        
+        System.out.println(name + "??!");
+    }
+}
